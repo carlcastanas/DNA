02189d08dfb84fac7ce1ea9812e257cc7bde4740
Carl Andrew Castañas <89068725+carlcastanas@users.noreply.github.com>
2023-01-22 16:42:48 +0800

    Update dna.py

--- dna.py
+++ dna.py
@@ -9,8 +9,11 @@ dna_sample = ''.join(random.choices(nucleotides, k=dna_length))
 # 1 byte = 8 bits
 # 1 nucleotide = 2 bits (since there are 4 possible nucleotides)
 # 1 megabyte = 10^6 bytes
+# 1 gigabyte = 10^9 bytes
 
-dna_sample_size = (dna_length * 2) / 8
-dna_sample_size_mb = dna_sample_size / (10 ** 6)
+dna_sample_size_bits = dna_length * 2
+dna_sample_size_bytes = dna_sample_size_bits / 8
+dna_sample_size_mb = dna_sample_size_bytes / (10 ** 6)
+dna_sample_size_gb = dna_sample_size_bytes / (10 ** 9)
 
-print("DNA Sample Size: {:.2f} megabytes".format(dna_sample_size_mb))
+print("DNA Sample Size: {:.2f} megabytes / {:.2f} gigabytes".format(dna_sample_size_mb, dna_sample_size_gb))
