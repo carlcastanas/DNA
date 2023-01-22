2bf70ab7bcebd559d99889b90b4ee394d389e9a9
Carl Andrew Castañas <89068725+carlcastanas@users.noreply.github.com>
2023-01-22 16:38:20 +0800

    Create dna.py

---
+++ dna.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+nucleotides = ['A', 'C', 'G', 'T']
+dna_length = 100000000  # 100 million nucleotides
+
+dna_sample = ''.join(random.choices(nucleotides, k=dna_length))
+
+# 1 byte = 8 bits
+# 1 nucleotide = 2 bits (since there are 4 possible nucleotides)
+# 1 megabyte = 10^6 bytes
+
+dna_sample_size = (dna_length * 2) / 8
+dna_sample_size_mb = dna_sample_size / (10 ** 6)
+
+print("DNA Sample Size: {:.2f} megabytes".format(dna_sample_size_mb))
